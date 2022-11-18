87b2d9f6dd548151fa4378728563dc6bc14ea92a
sneh-shin <snehashinde800@gmail.com>
2022-11-18 15:56:44 +0530

    created saveData handler and annotated with PostMapping

--- src/main/java/com/niit/jdp/controller/MovieController.java
+++ src/main/java/com/niit/jdp/controller/MovieController.java
@@ -5,8 +5,11 @@
  */
 package com.niit.jdp.controller;
 
+import com.niit.jdp.domain.Movie;
 import com.niit.jdp.service.MovieServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,11 @@ public class MovieController {
 
     public MovieController(MovieServiceImpl movieServiceImpl) {
         this.movieServiceImpl = movieServiceImpl;
+    }
+
+    @PostMapping("/insertData")
+    public String saveData(@RequestBody Movie movie) {
+        movieServiceImpl.saveData(movie);
+        return "Movie Added";
     }
 }
