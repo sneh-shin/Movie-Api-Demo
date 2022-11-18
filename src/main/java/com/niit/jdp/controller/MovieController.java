2abf586b61c9155a1c9c5cd9496faf34bc67b51c
sneh-shin <snehashinde800@gmail.com>
2022-11-18 15:51:05 +0530

    created object of the MovieServiceImpl.java in the MovieController.java and injected it in the constructor

--- src/main/java/com/niit/jdp/controller/MovieController.java
+++ src/main/java/com/niit/jdp/controller/MovieController.java
@@ -5,5 +5,19 @@
  */
 package com.niit.jdp.controller;
 
+import com.niit.jdp.service.MovieServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class MovieController {
+    private MovieServiceImpl movieServiceImpl;
+
+    @Autowired
+
+    public MovieController(MovieServiceImpl movieServiceImpl) {
+        this.movieServiceImpl = movieServiceImpl;
+    }
 }
