db79aa575d9c76312aab1cdd23ff46479cb5c6e3
sneh-shin <snehashinde800@gmail.com>
2022-11-18 15:32:06 +0530

    added getters and setters,toString() in the Movie.java

--- src/main/java/com/niit/jdp/domain/Movie.java
+++ src/main/java/com/niit/jdp/domain/Movie.java
@@ -25,4 +25,46 @@ public class Movie {
         this.actorName = actorName;
         this.directorName = directorName;
     }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieId=" + movieId +
+                ", movieName='" + movieName + '\'' +
+                ", actorName='" + actorName + '\'' +
+                ", directorName='" + directorName + '\'' +
+                '}';
+    }
 }
